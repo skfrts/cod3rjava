ae71eceade9450db9eace20e65ca5a9d6744f87e
Fabio Freitas <fabiofreitas.pj@gmail.com>
2022-10-20 14:00:12 -0300

    fix

--- exercicios/src/fundamentos/ConversaoStringNumero.java
+++ exercicios/src/fundamentos/ConversaoStringNumero.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 public class ConversaoStringNumero {
     
     public static void main(String[] args) {
+        
         String valor1 = JOptionPane.showInputDialog("Digite o primeiro número: ");
         String valor2 = JOptionPane.showInputDialog("Digite o segundo número: ");
         
